e990521d3e071114325d843ab6b041b221f6441f
Anthony Limere <anthonylimere@gmail.com>
2018-07-05 22:43:10 +0200

    Add child_process access

--- routes/reporterRoutes.js
+++ routes/reporterRoutes.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
+const util = require('util');
 
 module.exports = app => {
   app.post("/api/lighthouse_report", async (req, res) => {
-    let url
+    const exec = util.promisify(require('child_process').exec);
 
-    url = req.query.url;
+    async function terminal_command() {
+      const { stdout, stderr } = await exec('ls');
+      console.log('stdout:', stdout);
+      console.log('stderr:', stderr);
+    }
 
-    console.log(url)
+    await terminal_command();
+
+    res.send(stdout)
   })
 }
