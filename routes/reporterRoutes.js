58d97449534736b38342096365958b4b07d08648
Anthony Limere <anthonylimere@gmail.com>
2018-07-06 11:38:52 +0200

    Add lighthouse reporter read-write-get

--- routes/reporterRoutes.js
+++ routes/reporterRoutes.js
@@ -1,18 +1,54 @@
-const util = require('util');
+const util = require("util");
+const fs = require('fs');
 
 module.exports = app => {
-  app.post("/api/lighthouse_report", async (req, res) => {
-    const exec = util.promisify(require('child_process').exec);
+  app.get("/api/lighthouse_report", async (req, res) => {
+    let url, obj;
+    url = req.query.url;
 
-    async function terminal_command() {
-      const { stdout, stderr } = await exec('ls');
-      console.log('stdout:', stdout);
-      console.log('stderr:', stderr);
+    const exec = util.promisify(require("child_process").exec);
+
+    async function terminal_command(command, callback){
+      exec(command, {maxBuffer: 1024 * 500}, function(error, stdout, stderr){
+        console.log('running post-terminal command')
+        console.log('found file')
+        fs.readFile('./myfile.json', handleFile)
+
+        // Write the callback function
+        function handleFile(err, data) {
+          if (err) throw err
+          obj = JSON.parse(data)
+          let result = {}
+          result.performance = obj["categories"]["performance"]["score"]
+          result.pwa = obj["categories"]["pwa"]["score"]
+          result.accessibility = obj["categories"]["accessibility"]["score"]
+          result.bestPractices = obj["categories"]["best-practices"]["score"]
+          result.seo = obj["categories"]["seo"]["score"]
+          // You can now play with your datas
+          res.send(result)
+
+          fs.unlink('./myfile.json',function(err){
+            if(err) return console.log(err);
+            console.log('file deleted successfully');
+          });
+        }
+      });
+    };
+
+    await terminal_command(`lighthouse ${url} --output json --output-path ./myfile.json`, file_callback)
+
+    async function file_callback() {
+      console.log('running post-terminal command')
+      console.log('found file')
+      fs.readFile('./myfile.json', handleFile)
+
+      // Write the callback function
+      function handleFile(err, data) {
+        if (err) throw err
+        obj = JSON.parse(data)
+        console.log(obj)
+        // You can now play with your datas
+      }
     }
-
-    await terminal_command();
-
-    res.send(200)
-    res.send(stdout)
-  })
-}
+  });
+};
